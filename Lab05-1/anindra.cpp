0ed5e3aa969c2b124dfc03d818ee297d688adf9a
Aman Nindra <amannindra@gmail.com>
2026-03-05 23:41:48 -0800

    cd

---
+++ Lab05-1/anindra.cpp
@@ -0,0 +1,87 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void printVector(vector<int> array)
+{
+    cout << "[";
+    for (int i = 0; i < array.size(); i++)
+    {
+        cout << array[i] << ", ";
+    }
+
+    cout << "]" << endl;
+}
+
+vector<int> countingsort(vector<int> array)
+{
+    vector<int> count(4, 0);
+    for (int i = 0; i < array.size(); i++)
+    {
+        count[array[i]]++;
+    }
+
+    // cout << "Count Array: ";
+    // printVector(count);
+    // cout << endl;
+
+    vector<int> new_array = {};
+
+    for (int i = 0; i < count.size(); i++)
+    {
+        // cout << "count: " << i << ", " << "Count[i]: " << count[i] << endl;
+        for (int j = 0; j < count[i]; j++)
+        {
+            new_array.push_back(i);
+        };
+    };
+    return new_array;
+}
+
+vector<int> getColumn(const vector<vector<int>> &array, int col)
+{
+    vector<int> column;
+
+    for (int row = 0; row < array.size(); row++)
+    {
+        column.push_back(array[row][col]);
+    }
+
+    return column;
+}
+void sortArray(vector<vector<int>> array)
+{
+    vector<vector<int>> new_array;
+    for (int i = 0; i < array[0].size(); i++)
+    {
+        vector<int> each_array = getColumn(array, i);
+        vector<int> sorted = countingsort(each_array);
+        printVector(sorted);
+    }
+
+};
+int main()
+{
+    int num = 5;
+
+    vector<vector<int>> array = {
+        {3, 3, 3, 3, 3, 2, 2, 2, 2, 2},
+        {2, 3, 2, 2, 2, 2, 2, 2, 2, 2},
+        {1, 3, 0, 0, 2, 1, 0, 0, 0, 0},
+        {1, 3, 0, 0, 2, 2, 0, 0, 0, 0},
+        {2, 3, 2, 1, 2, 2, 2, 2, 2, 2},
+
+    };
+
+    vector<int> start = array[0];
+    cout << "Base array: ";
+    printVector(start);
+    cout << endl;
+    cout << "Count: ";
+    printVector(countingsort(start));
+
+    sortArray(array);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cs100-lab
+
+Run program:
+
+g++ -g anindra.cpp io anindra && ./anindra
+make
+chmod +x ./Grade
+./grade
